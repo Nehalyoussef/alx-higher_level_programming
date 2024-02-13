f56996af4525babb576b6fd8c8dcfd30b3bda2cd
Nehalyoussef <nehal.youssef@ejust.edu.eg>
2024-02-12 17:53:32 -0800

    adding getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,11 @@ class Square(Rectangle):
         '''Returns string info about this square.'''
         return '[{}] ({}) {}/{} - {}'.format(
             type(self).__name__, self.id, self.x, self.y, self.width)
+    @property
+    def size(self):
+        '''size of the square'''
+        return self.width
+    @size.setter
+    def size(self,value):
+        self.width = value
+        self.height = value
